20618d37565f6c36e155bf9bd347cd629c715a96
abenben <abenbenben@gmail.com>
2021-12-19 09:50:03 +0900

    追加

---
+++ src/main_004.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let x = String::from("Hello");
+    let y = string_length(&x);
+    println!("{} is {}", x, y);
+    let name = "masuda";
+    let age = get_person_age();
+    if age > 20 {
+        let age = 100;
+    }
+    println!("name is {}, age {}", name, age);
+}
+
+fn string_length(x: &String) -> usize {
+    let length = x.len();
+    length
+}
+
+fn get_person_age() -> i32 {
+    30
+}
