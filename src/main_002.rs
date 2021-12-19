0d35d4f2387adc3ecb195d2adc0305ed409c664f
abenben <abenbenben@gmail.com>
2021-12-19 09:00:02 +0900

    修正

--- src/main_002.rs
+++ src/main_002.rs
@@ -5,6 +5,17 @@ fn main() {
     println!("{}", name);
     println!("{}", age);
     println!("{}", add(5, 10));
+
+    let s1 = String::from("Hello");
+    let s2 = String::from("Rust");
+    let s3 = String::from("World.");
+    let s = s1 + " " + &s2 + " " + &s3;
+    println!("{}", s);
+    let s1 = String::from("Hello");
+    let s2 = String::from("Rust");
+    let s3 = String::from("World.");
+    let s = format!("{} {} {}", s1, s2, s3);
+    print!("{}", s);
 }
 
 fn add(x: i32, y: i32) -> i32 {
