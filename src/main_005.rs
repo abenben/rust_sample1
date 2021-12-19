79011adafb6658e8a78b76f8b99ed30dbbe461c1
abenben <abenbenben@gmail.com>
2021-12-19 10:22:09 +0900

    修正

--- src/main_005.rs
+++ src/main_005.rs
@@ -1,3 +1,24 @@
 fn main() {
+    let s1 = "Hello, ";
+    let s2 = "World!";
+    let s3 = s1.to_string() + s2;
+    assert_eq!(s3, "Hello, World!");
 
+    let ret = hello();
+    assert_eq!(ret, ());
+
+    assert_eq!(std::mem::size_of::<()>(), 0);
+    assert_eq!(std::mem::size_of::<bool>(), 1);
+
+    // aaaa
+    /* aaaa
+       nnn
+     */
+    /*
+    /* aaaa */
+    */
+}
+
+fn hello() {
+    println!("Hello");
 }
