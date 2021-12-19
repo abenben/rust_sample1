df97673828b73b473bbfe584d4acfe28b8b191dc
abenben <abenbenben@gmail.com>
2021-12-19 17:03:50 +0900

    修正

---
+++ src/sample_attribute.rs
@@ -0,0 +1,28 @@
+#[test]
+fn test1(){
+    println!("test1");
+}
+
+// fn test2(){
+//     #![test]
+//     println!("test2");
+// }
+
+//#[macro_use]mod macros;
+
+//use log::[debug,error];
+
+#[cfg(unix)]
+fn something_for_unix(){
+    println!("unix")
+}
+
+#[cfg(windows)]
+fn something_for_windows(){
+    println!("windows")
+}
+
+fn main() {
+    //test1();
+    //test2();
+}
