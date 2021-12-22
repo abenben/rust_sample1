c2ebdaf98158673f943509a1082ebd0a9b990994
abenben <abenbenben@gmail.com>
2021-12-22 14:19:36 +0900

    修正

--- src/sample_cloujor.rs
+++ src/sample_cloujor.rs
@@ -11,4 +11,19 @@ fn main() {
     };
     one += 1;
     println!("10 + 1 = {}", plus_one(10));
+
+    const N_THREADS: usize = 3;
+    let series_range = 0..30;
+    let add = 1;
+
+    let chunks = (0..N_THREADS)
+        .map(|ii| series_range.clone().skip(ii).step_by(N_THREADS));
+
+    let handles: Vec<_> = chunks
+        .map(|vv| std::thread::spawn(move || {
+            vv.for_each(|nn| println!("{}", nn + add));
+        })
+        ).collect();
+
+    handles.into_iter().for_each(|hh| hh.join().unwrap());
 }
